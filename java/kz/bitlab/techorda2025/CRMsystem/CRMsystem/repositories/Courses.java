b69f26de8a2c5267a78b32e2ee4b16d05243d7d7
ruslann-zh <84094995+ruslann-zh@users.noreply.github.com>
2025-03-04 13:21:17 +0500

    Update Courses.java

--- java/kz/bitlab/techorda2025/CRMsystem/CRMsystem/repositories/Courses.java
+++ java/kz/bitlab/techorda2025/CRMsystem/CRMsystem/repositories/Courses.java
@@ -16,4 +16,6 @@ public class Courses {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private int price;
 }
